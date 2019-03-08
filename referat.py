b3299988f09e9c1345284a12acb26c638b1f1b9e
E A <elena7263827+py@gmail.com>
2019-03-08 19:55:16 +0300

    Reading, replacing and writing in a new file

---
+++ referat.py
@@ -0,0 +1,8 @@
+with open('referat.txt', 'r', encoding='utf-8') as file_to_read:
+    content = file_to_read.read()
+    print(f"В тексте {len(content)} символов")
+    print(f"В тексте {len(content.split())} слов")
+    content = content.replace('.', '!')
+
+with open('referat2.txt', 'w', encoding='utf-8') as file_to_write:
+    file_to_write.write(content)
