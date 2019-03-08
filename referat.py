b43943ac6ac6fce3d58bbc581993a3eb39f76ba9
E A <elena7263827+py@gmail.com>
2019-03-08 20:01:15 +0300

    empty line, branches sorting out

--- referat.py
+++ referat.py
@@ -4,5 +4,6 @@ with open('referat.txt', 'r', encoding='utf-8') as file_to_read:
     print(f"В тексте {len(content.split())} слов")
     content = content.replace('.', '!')
 
+
 with open('referat2.txt', 'w', encoding='utf-8') as file_to_write:
     file_to_write.write(content)
